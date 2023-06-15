3f33e52dfdf8532000c94bd23f6a3855f6e719a9
martin <marko.hoff123@gmail.com>
2023-06-16 00:14:32 +0200

    * minor changes

--- src/main/java/com/ulaf/ste/ordering_system/Model/Category.java
+++ src/main/java/com/ulaf/ste/ordering_system/Model/Category.java
@@ -5,11 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class Category {
 
     @Id
