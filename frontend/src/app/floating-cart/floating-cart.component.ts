fc5faf9e42ad79f6a5dd89aefe90961558080e7d
martin <marko.hoff123@gmail.com>
2023-06-18 23:07:39 +0200

    changes

---
+++ frontend/src/app/floating-cart/floating-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-cart',
+  templateUrl: './floating-cart.component.html',
+  styleUrls: ['./floating-cart.component.css']
+})
+export class FloatingCartComponent {
+
+}
