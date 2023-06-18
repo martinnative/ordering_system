0e58b681ba7872b9e03e8d932e76b9f65f55e154
martin <marko.hoff123@gmail.com>
2023-06-18 19:39:56 +0200

    - added and updated Angular components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
+import {MenuComponent} from "./menu/menu.component";
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
-  { path: 'second', component: HomeComponent}
+  { path: 'second', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'menu', component: MenuComponent}
+
+
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,15 @@ import { InstagramComponent } from './instagram/instagram.component';
 import { HowWeDoItComponent } from './how-we-do-it/how-we-do-it.component';
 import { MakeYourOwnPizzaComponent } from './make-your-own-pizza/make-your-own-pizza.component';
 import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { MobileNavigationComponent } from './mobile-navigation/mobile-navigation.component';
+import { CartComponent } from './cart/cart.component';
+import { FloatingCartComponent } from './floating-cart/floating-cart.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -22,11 +31,20 @@ import { BannerComponent } from './banner/banner.component';
     HowWeDoItComponent,
     MakeYourOwnPizzaComponent,
     BannerComponent,
+    AboutComponent,
+    MenuComponent,
+    ContactComponent,
+    HeaderComponent,
+    MobileNavigationComponent,
+    CartComponent,
+    FloatingCartComponent,
+    SearchFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlickCarouselModule
+    SlickCarouselModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
