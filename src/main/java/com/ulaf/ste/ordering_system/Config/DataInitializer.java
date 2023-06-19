18eef090e88c840ce92d22ff3b8a427020e50968
fetzy <fetzytv@gmail.com>
2023-06-19 23:20:58 +0200

    minor fixes

--- src/main/java/com/ulaf/ste/ordering_system/Config/DataInitializer.java
+++ src/main/java/com/ulaf/ste/ordering_system/Config/DataInitializer.java
@@ -58,9 +58,9 @@ public class DataInitializer {
 
         byte[] image1 = getPizzaImageBytes("margarita.png");
         byte[] image2 = getPizzaImageBytes("kaprichioza.png");
-        productService.createProduct(new Product("Margarita Pizza", 280.0,"Pizza with cheese and ketchup",true, productIngredients, image1, category));
-        productService.createProduct(new Product("Kaprichioza Pizza", 320.0,"Pizza with ham, cheese and mushrooms",true, productIngredients2, image2, category));
-        productService.createProduct(new Product("Coca Cola",120.0,"Cold drink",false,new ArrayList<>(),image1,cat2));
+        productService.createProduct(new Product("Margarita Pizza", 280.0,"Pizza with cheese and ketchup",true,true, productIngredients, image1, category));
+        productService.createProduct(new Product("Kaprichioza Pizza", 320.0,"Pizza with ham, cheese and mushrooms",true,true, productIngredients2, image2, category));
+        productService.createProduct(new Product("Coca Cola",120.0,"Cold drink",false,true,new ArrayList<>(),image1,cat2));
 
         List<OrderItem> listItems = new ArrayList<>();
         product_qtyRepository.save(new OrderItem(1L,productService.getProductById(1L), 2));
