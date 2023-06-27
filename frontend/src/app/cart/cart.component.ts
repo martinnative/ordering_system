db252b0e00d694d0b44e7933f816a1eac924ad6a
martin <marko.hoff123@gmail.com>
2023-06-28 00:24:50 +0200

    - updated functionality in cart component (made dynamic)

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ShoppingCartService} from "../shopping-cart.service";
+import {ImageService} from "../image.service";
+import {Product} from "../../model/Product";
+import {OrderItem} from "../../model/OrderItem";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit, OnChanges {
+  cartItems: OrderItem[] = [];
+  constructor(private shoppingCartService:ShoppingCartService,private imageService:ImageService
+  ) {};
+
+  transformData(data: Product):Product {
+    return this.imageService.transformData(data);
+  }
+  ngOnInit(): void {
+    this.cartItems = this.shoppingCartService.getCartItems();
+  }
+  calculateTotal(): number {
+    return this.shoppingCartService.calculateTotal();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.cartItems = this.shoppingCartService.getCartItems();
+  }
+
+  removeFromCart(orderItem: OrderItem){
+    this.cartItems = this.shoppingCartService.removeFromCart(orderItem);
+  }
   quantity:number = 1;
 
   subQuantity() {
