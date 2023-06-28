d8bd0af6985086bfbad29d02946a3c2f797cffd2
martin <marko.hoff123@gmail.com>
2023-06-29 01:53:11 +0200

    * minor changes

--- src/main/java/com/ulaf/ste/ordering_system/Model/Image.java
+++ src/main/java/com/ulaf/ste/ordering_system/Model/Image.java
@@ -1,12 +1,16 @@
 package com.ulaf.ste.ordering_system.Model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +27,15 @@ public class Image {
         this.type = type;
     }
 
-    public Image() {
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Image image = (Image) o;
+        return getId() != null && Objects.equals(getId(), image.getId());
     }
 }
