db7da665312f11b589bb1244f7c6fb6498e814af
martin <marko.hoff123@gmail.com>
2023-06-28 12:16:10 +0200

    - fixed related products in product single componet * minor changes in product single component

--- frontend/src/app/product-single/product-single.component.ts
+++ frontend/src/app/product-single/product-single.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from "@angular/router";
 import {filter, map, mergeMap, tap} from "rxjs";
 import {ImageService} from "../image.service";
 import {ShoppingCartService} from "../shopping-cart.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CustomizeModalComponent} from "../customize-modal/customize-modal.component";
 
 @Component({
   selector: 'app-product-single',
@@ -14,6 +16,7 @@ import {ShoppingCartService} from "../shopping-cart.service";
 export class ProductSingleComponent implements OnInit {
 
   product: Product | undefined = undefined;
+  products: Product[] = [];
   _id: String = "";
 
   constructor(
@@ -21,6 +24,8 @@ export class ProductSingleComponent implements OnInit {
     private productsService: ProductsService,
     private imageService: ImageService,
     private shoppingCartService: ShoppingCartService,
+    private productService:ProductsService,
+    private modalService: NgbModal,
 
   ) { }
 
@@ -33,11 +38,24 @@ export class ProductSingleComponent implements OnInit {
       tap(data => console.log(data))
     )
       .subscribe(data => this.product = data);
+    this.productsService.findAllProducts().subscribe(products => {
+      this.products = products;
+    });
   }
   sanitizeImage(product:Product):Product {
     return this.imageService.transformData(product);
   }
   addToCart(product: Product) {
     this.shoppingCartService.addToCart(product);
+  }
+  openModal(product: Product) {
+    console.log("product");
+    const modalRef = this.modalService.open(CustomizeModalComponent);
+    modalRef.componentInstance.product = product;
+    modalRef.result.then((result) => {
+      if (result) {
+        console.log(result)
+      }
+    });
   }
 }
